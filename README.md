d16ce4b30971dc9aed97021060ef20a61fc1af2f
Mohammad Mahdi <noeimohammadmahdi@gmail.com>
2023-10-25 15:16:05 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Typescript MERN ECommerce
+
+## roadmap
+1. Frontend using ReactJS 
+2. Backend using ExpressJS
+3. Database using mongodb
+
---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        ECommerce
+      </header>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
+
+export default App
