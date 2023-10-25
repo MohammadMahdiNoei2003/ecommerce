7de4ca8f0b32f7eb2af05da26d74445ce1480b6f
Mohammad Mahdi <noeimohammadmahdi@gmail.com>
2023-10-25 18:14:52 +0330

    making link to product page from products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
-import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
-import { sampleProducts } from './data'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
+
 
 function App() {
 
@@ -14,22 +15,13 @@ function App() {
           </Container>
           <Nav>
             <a href='/cart' className='nav-link'>Cart</a>
-            <a href='/singin' className='nav-link'>Sign In</a>
+            <a href='/sing-in' className='nav-link'>Sign In</a>
           </Nav>
         </Navbar>
       </header>
       <main>
         <Container className='mt-3'>
-          <Row>
-            {
-              sampleProducts.map(product => (
-                <Col key={product.slug} sm={6} md={4} lg={3}>
-                  <img src={product.image} alt={product.name} className='product-image' />
-                  <h4>{product.name}</h4>
-                  <p>Price: {product.price} $</p>
-                </Col>))
-            }
-          </Row>
+          <Outlet />
         </Container>
       </main>
       <footer>
