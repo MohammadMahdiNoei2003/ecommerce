a49f6690ae8f2591f1c8481e67cbf135ebf85c0e
Mohammad Mahdi <noeimohammadmahdi@gmail.com>
2023-10-25 17:48:23 +0330

    changing style of list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,41 @@
-import './App.css'
+import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
 import { sampleProducts } from './data'
 
 function App() {
 
   return (
-    <div>
+    <div className='d-flex flex-column vh-100'>
       <header>
-        ECommerce
+        <Navbar bg='dark' variant='dark' expand='lg'>
+          <Container>
+            <Navbar.Brand>
+              E-COMMERCE
+            </Navbar.Brand>
+          </Container>
+          <Nav>
+            <a href='/cart' className='nav-link'>Cart</a>
+            <a href='/singin' className='nav-link'>Sign In</a>
+          </Nav>
+        </Navbar>
       </header>
       <main>
-        <ul>
-          {
-            sampleProducts.map(product => (
-            <li key={product.slug}>
-              <h2>{product.name}</h2>
-              <img src={product.image} alt={product.name} className='product-image' />
-              <p>Price: {product.price} $</p>
-            </li>))
-          }
-        </ul>
+        <Container className='mt-3'>
+          <Row>
+            {
+              sampleProducts.map(product => (
+                <Col key={product.slug} sm={6} md={4} lg={3}>
+                  <img src={product.image} alt={product.name} className='product-image' />
+                  <h4>{product.name}</h4>
+                  <p>Price: {product.price} $</p>
+                </Col>))
+            }
+          </Row>
+        </Container>
       </main>
       <footer>
-          Information
+        <div className='text-center'>
+          All right reserved
+        </div>
       </footer>
     </div>
   )
